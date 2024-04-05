68a1b771c03463107ad2e2f274d9c96c9875daea
ajirohimat <ajirohimat03@gmail.com>
2024-04-05 13:26:28 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/aihpraktikumjf2/praktikum2/Praktikum2.java
@@ -0,0 +1,13 @@
+package com.juaracoding.aihpraktikumjf2.praktikum2;
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-233.15026.9, built on March 21, 2024
+@Author ajirohimat aih a.k.a Aji Rohimat
+Java Developer
+Created on 05/04/24 11.09
+@Last Modified 05/04/24 11.09
+Version 1.0
+*/
+
+public class Praktikum2 {
+}
